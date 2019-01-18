f5bc30b0608a5559f32491271ba10bab9e4def02
Madi <madelyn.e.helton@gmail.com>
2019-01-18 04:31:08 -0600

    working on allowing managers to adjust quantities of products

--- bamazonManager.js
+++ bamazonManager.js
@@ -69,10 +69,22 @@ let inventory = {
   },
 
   lowQuantity: function(){
+    connection.query('SELECT * FROM products WHERE stock_quantity <= 5' , function(error, response){
+      
+      if(response.length == 0){
+        console.log("\n*No products are running low at this time.*\n");
+        return inventory.menu();
+      };
 
+      for(let i = 0; i < response.length; i ++){
+        console.log(response[i].item_id + ": " + response[i].product_name + " - " + response[i].department_name + " | Price: " + response[i].price + " | Quantity: " + response[i].stock_quantity);
+      };
+      return inventory.menu();
+    });
   },
 
   addQuantity: function(){
+    
 
   },
 
